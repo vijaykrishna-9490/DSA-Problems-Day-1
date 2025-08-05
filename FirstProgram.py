11082e68563da8a2f74e3585cd5418b3316d61e8
vijaykrishna-9490 <vijaykrishna8688@gmail.com>
2025-08-05 12:17:40 +0530

    Initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,4 @@
+str1 = "All_sliver_tea_cups."
+n = input().lower().split()
+n = "_".join(n)
+print(n)
---
+++ Second.py
@@ -0,0 +1,4 @@
+str1 = "All sliver tea cups."
+n = input().title().split()
+n = " ".join(n)
+print(n)
